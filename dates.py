5c6118dac30228d56ae42e58e8e3de35630233e9
Sam G <thegladiusmaximus@gmail.com>
2014-11-09 21:37:50 -0600

    fixed date bot

--- dates.py
+++ dates.py
@@ -22,35 +22,39 @@ def format_dates(file_name):
     #create_date = datetime.datetime.strptime(createdate_txt[:19], '%Y-%m-%d %H:%M:%S')
     return c_date_txt[:19], m_date.strftime('%Y-%m-%d %H:%M:%S')
 
-for line in git('--no-pager', 'diff', '--name-only', '--no-color', 'HEAD~1', 'HEAD~2', _iter=True):
-    file_name = './' + line.strip()
+def run()
+    for line in git('--no-pager', 'diff', '--name-only', '--no-color', 'HEAD~1', 'HEAD~2', _iter=True):
+        file_name = './' + line.strip()
 
-    try:
-        ls(file_name) # test if file exists
-    except:
-        print ('{file_name} is not present'.format(**locals()))
-        continue
-    if '.md' not in file_name:
-        print ('{file_name} is not a markdown file'.format(**locals()))
-        continue
-        
-    with open(file_name, 'r') as file_obj:
-        file_contents = file_obj.read()
-        
-    a, b, m = insertion_point(file_contents)
-    if not (a and b):
-        print ('{file_name} is not formatted for a date'.format(**locals()))
-        continue
+        try:
+            ls(file_name) # test if file exists
+        except:
+            print ('{file_name} is not present'.format(**locals()))
+            continue
+        if '.md' not in file_name:
+            print ('{file_name} is not a markdown file'.format(**locals()))
+            continue
 
-    print ('{file_name} putting date {m}'.format(**locals()))
-    c_date, m_date = format_dates(file_name)
-    file_contents = (
-        file_contents[:a] + 
-        'Created: ' + c_date + '\n' +
-        'Last modified: ' + m_date + '\n' +
-        file_contents[b:])
+        with open(file_name, 'r') as file_obj:
+            file_contents = file_obj.read()
 
-    with open(file_name, 'w') as file_obj:
-        file_obj.write(file_contents)
-    
+        a, b, m = insertion_point(file_contents)
+        if not (a and b):
+            print ('{file_name} is not formatted for a date'.format(**locals()))
+            continue
 
+        print ('{file_name} putting date {m}'.format(**locals()))
+        c_date, m_date = format_dates(file_name)
+        file_contents = (
+            file_contents[:a] + '\n'
+            'Created: ' + c_date + '\n\n' +
+            'Last modified: ' + m_date + '\n\n' +
+            file_contents[b:])
+
+        with open(file_name, 'w') as file_obj:
+            file_obj.write(file_contents)
+
+if str(git('--no-pager', 'show', 'HEAD', '--format="%s"', '-s')).strip() != 'date bot':
+    run()
+    git('commit', '--all', '--message="date bot"')
+    git('push')
